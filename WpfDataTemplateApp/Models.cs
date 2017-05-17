a17426aecf6bb313dcb6ff22c6eb15038914a4b3
giganoide <giganoide@hotmail.com>
2017-05-17 15:29:57 +0200

    Add project files.

---
+++ WpfDataTemplateApp/Models.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDataTemplateApp
+{
+    public class Tornio : IMacchina
+    {
+        public string TypeLabel => "Tornio";
+        public string Prova => "Prova";
+    }
+
+    public class Fresa : IMacchina
+    {
+        public string TypeLabel => "Fresa";
+    }
+
+    public class Combinato : IMacchina
+    {
+        public string TypeLabel => "Combinato";
+    }
+
+    public interface IMacchina
+    {
+        string TypeLabel { get; }
+    }
+}
